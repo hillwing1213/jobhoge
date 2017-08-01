498d64dbe7aa16765434a5f2578d1108284ca236
AMANO <AMANO@192.168.202.21>
2017-08-01 16:37:09 +0900

    test

--- src/pack/Hello.java
+++ src/pack/Hello.java
@@ -1,12 +1,11 @@
 package pack;
 
 public class Hello {
-
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("tanaka");
 
-Tanaka aaa = new Tanaka();
-aaa.method();
+		Tanaka aaa = new Tanaka();
+		aaa.method();
 	}
 }
